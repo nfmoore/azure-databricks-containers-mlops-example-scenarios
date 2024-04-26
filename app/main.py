2ea87fd7b7ae8c240163271d94f19d2f745e8a33
Nicholas Moore <nfmoore@outlook.com>
2024-04-26 06:35:27 +0000

    Update app

--- app/main.py
+++ app/main.py
@@ -90,4 +90,4 @@ async def predict(data: list[LoanApplicant]) -> str:
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
